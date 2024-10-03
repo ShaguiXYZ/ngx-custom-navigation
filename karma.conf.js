89124a0b4de355ae78c7e7e6de5b46f49a4a2214
Shagui <shaguixyz@gmail.com>
2024-10-03 11:43:09 +0200

    karma conf

---
+++ karma.conf.js
@@ -0,0 +1,48 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+// enabled this flag to remove skipped tests from the output
+const COMPACT = true;
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      'karma-chrome-launcher',
+      require('karma-jasmine'),
+      // require('@chiragrupani/karma-chromium-edge-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('karma-mocha-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      reporters: [
+        { type: 'html', subdir: './' },
+        { type: 'text-summary', subdir: './' },
+        { type: 'lcovonly', subdir: './' }
+      ]
+    },
+    reporters: ['mocha'],
+    mochaReporter: {
+      ignoreSkipped: COMPACT
+    },
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadlessCustom'],
+    singleRun: true,
+    customLaunchers: {
+      ChromeHeadlessCustom: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    }
+  });
+};
