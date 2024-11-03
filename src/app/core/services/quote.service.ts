d1f704075091771a4859bb1238b905c7ef1f40c2
Shagui <shaguixyz@gmail.com>
2024-11-03 23:11:26 +0100

    update context data from quote configuration json

--- src/app/core/services/quote.service.ts
+++ src/app/core/services/quote.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { ContextDataService, deepCopy } from '@shagui/ng-shagui/core';
 import { QUOTE_APP_CONTEXT_DATA } from '../constants';
-import { AppContextData } from '../models';
+import { AppContextData, QuoteComponent } from '../models';
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +9,17 @@ import { AppContextData } from '../models';
 export class QuoteService {
   private contextDataService = inject(ContextDataService);
 
-  public loadComponentData = <T extends object>(component: T): void => {
+  public loadComponentData = <T extends QuoteComponent>(component: T): void => {
     const contextData = this.contextDataService.get<AppContextData>(QUOTE_APP_CONTEXT_DATA);
     const lastPage = contextData?.navigation.lastPage;
-    const pageData = { ...{}, ...lastPage?.configuration?.data };
+    const pageData = { ...lastPage?.configuration?.data };
 
-    Object.entries(pageData).forEach(
-      ([key, value]) => Object.prototype.hasOwnProperty.call(component, key) && Object.assign(component, { [key]: deepCopy(value) })
-    );
+    Object.entries(pageData).forEach(([key, value]) => {
+      if (key === 'contextData' && typeof component['contextData'] === 'object' && typeof value === 'object') {
+        Object.assign(component, { [key]: { ...component['contextData'], ...value } });
+      } else if (Object.prototype.hasOwnProperty.call(component, key)) {
+        Object.assign(component, { [key]: deepCopy(value) });
+      }
+    });
   };
 }
